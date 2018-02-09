c58c3b6e1fe684217d56ffa240be975db7f2d9df
Sameer Subba <sameer.libang@gmail.com>
2018-02-09 18:53:13 +1100

    removing react-router and installing react-router-dom

--- src/routing-example/App.js
+++ src/routing-example/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import Router from 'react-router/BrowserRouter';
+import Match from 'react-router/Match';
+import Link from 'react-router/Link';
+import Redirect from 'react-router/Redirect';
 
+
+/*
 const Match = ({pattern, component: Component}) => {
   const pathname = window.location.pathname;
   if(pathname.match(pattern)) {
@@ -11,35 +17,45 @@ const Match = ({pattern, component: Component}) => {
   }
 };
 
-class App extends Component {
-  render() {
-    return (
-      <div className='ui text container'>
-        <h2 className='ui dividing header'>
-          Which body of water?
-        </h2>
+const Link = ({ to, children }) => (
+  <a
+    onClick={(e) => {
+      e.preventDefault();
+      history.push(to);
+    }}
+    href={to}
+  >
+    {children}
+  </a>
+)*/
 
-        <ul>
-          <li>
-            <a href='/atlantic'>
-              <code>/atlantic</code>
-            </a>
-          </li>
-          <li>
-            <a href='/pacific'>
-              <code>/pacific</code>
-            </a>
-          </li>
-        </ul>
+const App = () => (
+  <Router>
+    <div className='ui text container'>
+      <h2 className='ui dividing header'>
+        Which body of water?
+      </h2>
 
-        <hr />
-        {/* We'll insert the Match component here */}
-        <Match pattern='/atlantic' component={Atlantic} />
-        <Match pattern='/pacific' component={Pacific} />
-      </div>
-    );
-  }
-}
+      <ul>
+        <li>
+          <Link to='/atlantic'>
+            <code>/atlantic</code>
+          </Link>
+        </li>
+        <li>
+          <Link to='/pacific'>
+            <code>/pacific</code>
+          </Link>
+        </li>
+      </ul>
+
+      <hr />
+      {/* We'll insert the Match component here */}
+      <Match pattern='/atlantic' component={Atlantic} />
+      <Match pattern='/pacific' component={Pacific} />
+    </div>
+  </Router>
+);
 
 const Atlantic = () => (
   <div>
